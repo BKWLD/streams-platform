887df64098ae93ca022ec217e1feccb40fe359e8
Michael Starks <michael.starks@bkwld.com>
2020-01-21 14:57:54 -0800

    update to translations to show correct multiple field type

--- resources/js/form/translations.js
+++ resources/js/form/translations.js
@@ -19,6 +19,7 @@ $(function () {
         var selected = $(this);
         var locale = selected.attr('lang');
         var form = selected.closest('form');
+        var featuredTypes = $('[class*="featured"].multiple-field_type');
 
         var triggers = form.find('[data-toggle="lang"]');
         var group = triggers.closest('.btn-group');
@@ -32,5 +33,68 @@ $(function () {
 
         form.find('.form-group[lang]').addClass('hidden');
         form.find('.form-group[lang="' + locale + '"]').removeClass('hidden');
+
+        var toggleFeaturedTypesLocale = function(locale) {
+            $('.featured_' + locale + '-field').parent().show();
+            $('[class*="featured"]:not(.featured_' + locale +'-field).multiple-field_type').each(function (i, el) {
+                $(el).parent().hide();
+            });
+        }
+
+        switch (locale) {
+            case 'ca':
+                toggleFeaturedTypesLocale('ca')
+                break;
+            case 'fr-ca':
+                toggleFeaturedTypesLocale('fr-ca')
+                break;
+            case 'de':
+                toggleFeaturedTypesLocale('de')
+                break;
+            case 'be':
+                toggleFeaturedTypesLocale('be')
+                break;
+            case 'se':
+                toggleFeaturedTypesLocale('se')
+                break;
+            case 'gb':
+                toggleFeaturedTypesLocale('gb')
+                break;
+            case 'fr':
+                toggleFeaturedTypesLocale('fr')
+                break;
+            case 'pt':
+                toggleFeaturedTypesLocale('pt')
+                break;
+            case 'nl':
+                toggleFeaturedTypesLocale('nl')
+                break;
+            case 'au':
+                toggleFeaturedTypesLocale('au')
+                break;
+            case 'es':
+                toggleFeaturedTypesLocale('es')
+                break;
+            case 'it':
+                toggleFeaturedTypesLocale('it')
+                break;
+            case 'fi':
+                toggleFeaturedTypesLocale('fi')
+                break;
+            case 'en':
+                $('.featured-field').parent().show();
+                $('[class*="featured"]:not(.featured-field).multiple-field_type').each(function (i, el) {
+                    $(el).parent().hide();
+                });
+                break;
+            default:
+                console.log('no locale found');
+        }
+
+    });
+
+    // Show only the EN featured field
+    $('[class*="featured"]:not(.featured-field).multiple-field_type').each(function (i, el) {
+        $(el).parent().hide();
     });
 });
